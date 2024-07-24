911b0b0994eee4339677a5c13a3047a52e207cab
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-07-24 22:05:23 +0200

    Add prettier config

--- src/commands/garden/showField.js
+++ src/commands/garden/showField.js
@@ -1,25 +1,27 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { getOrCreateUser } = require('../../garden/user');
-const { getOrCreateField } = require('../../garden/field');
+const { SlashCommandBuilder } = require('discord.js')
+const { getOrCreateUser } = require('../../garden/user')
+const { getOrCreateField } = require('../../garden/field')
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('show')
-		.setDescription('Shows your field'),
-	async execute(interaction) {
-		const discordUser = interaction.user.id
+    data: new SlashCommandBuilder()
+        .setName('show')
+        .setDescription('Shows your field'),
+    async execute(interaction) {
+        const discordUser = interaction.user.id
         const user = await getOrCreateUser(discordUser)
         const field = await getOrCreateField(user.id)
 
-        let fieldString = ""
+        let fieldString = ''
         for (let i = 0; i < field.crops.length; i++) {
             const crop = field.crops[i]
             const icon = crop.spiecies.icon
-            fieldString += icon + " "
+            fieldString += icon + ' '
             if (i % 5 == 4) {
-                fieldString += "\n";
+                fieldString += '\n'
             }
         }
-		await interaction.reply({ content: fieldString, ephemeral: true });
-	},
-};
+        await interaction.reply({ content: fieldString, ephemeral: true })
+
+        console.log(field)
+    },
+}
--- src/garden/field.js
+++ src/garden/field.js
@@ -1,24 +1,25 @@
-const { db } = require("../db")
+const { db } = require('../db')
 
 module.exports = {
-    getOrCreateField: async (userId) => {
-        let field = await db.field.findUnique({ where: { userId: userId } })
-        if (!field) {
-            field = await db.field.create({
-                data: {
-                    userId: userId
-                }
-            })
+   getOrCreateField: async (userId) => {
+      let field = await db.field.findUnique({ where: { userId: userId } })
+      if (!field) {
+         field = await db.field.create({
+            data: {
+               userId: userId,
+            },
+         })
 
-            let defaultCrops = Array(20).fill({ fieldId: field.id, spiecesId: 0 })
-            await db.crop.createMany(
-                {
-                    data: defaultCrops
-                }
-            )
+         let defaultCrops = Array(20).fill({
+            fieldId: field.id,
+            spiecesId: 0,
+         })
+         await db.crop.createMany({
+            data: defaultCrops,
+         })
 
-            field = await db.field.findUnique({ where: { id: field.id } })
-        }
-        return field
-    }
+         field = await db.field.findUnique({ where: { id: field.id } })
+      }
+      return field
+   },
 }
